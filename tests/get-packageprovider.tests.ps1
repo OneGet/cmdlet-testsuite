34133cad55db481c3b0358380c72fdcb20f3a6f6
Garrett Serack <gserack@gmail.com>
2014-12-05 12:52:42 -0800

    renamed utility to module

--- tests/get-packageprovider.tests.ps1
+++ tests/get-packageprovider.tests.ps1
@@ -12,7 +12,7 @@
 #  limitations under the License.
 #  
 # ------------------ OneGet Test  ----------------------------------------------
-ipmo "$PSScriptRoot\utility.ps1"
+ipmo "$PSScriptRoot\utility.psm1"
 
 
 # ------------------------------------------------------------------------------
